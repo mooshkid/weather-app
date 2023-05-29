c8b7de16ac5dc47c290a4b4d6005de45e3544c53
lc-masa <myamanakalife6911@gmail.com>
2023-05-29 10:26:27 +0900

    removed extra js and added remove('fadeIn')

--- index.js
+++ index.js
@@ -4,17 +4,22 @@ const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
 
-search.addEventListener('click', () => {
+
+function doSearch() {
+
+  weatherBox.classList.remove('fadeIn');
+  weatherDetails.classList.remove('fadeIn');
 
   const APIKey = 'e385167daf2ffb5362a1e3235ce37fc2';
-  // const APIKey = '728b0ee6df5687559812bd3169ad77b7';
   const city = document.querySelector('.search-box input').value;
 
-  if (city === '')
+  if (city === '') {
+    container.style.height = '105px';
     return;
+  }
 
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then((response) => response.json()).then(json => {
-
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`).then((response) => response.json()).then(json => {
+    console.log(json);
     if (json.cod === '404') {
       container.style.height = '400px';
       weatherBox.style.display = 'none';
@@ -56,19 +61,39 @@ search.addEventListener('click', () => {
 
     }
 
-    temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+    // json data is all strings
+    // parseInt converts the input string to an integer by truncating any decimal places.
+    temperature.innerHTML = `${parseInt(json.main.temp)}<span>°F</span>`;
+
+    // to 1 decimal place
+    // temperature.innerHTML = `${parseFloat(json.main.temp).toFixed(1)}<span>°F</span>`;
+
     description.innerHTML = `${json.weather[0].description}<span>`;
     humidity.innerHTML = `${json.main.humidity}%`;
-    wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+    wind.innerHTML = `${parseInt(json.wind.speed)}mph`;
 
     weatherBox.style.display = '';
     weatherDetails.style.display = '';
     weatherBox.classList.add('fadeIn');
     weatherDetails.classList.add('fadeIn');
     container.style.height = '590px';
-
-
   });
 
 
+};
+
+
+
+const searchInput = document.querySelector('.search-box input');
+const searchButton = document.querySelector('.search-box button');
+
+
+searchButton.addEventListener('click', function() {
+  doSearch();
+});
+
+searchInput.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    doSearch();
+  }
 });
